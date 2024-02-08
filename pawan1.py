5261b7feda7b66042861296ccad8c28e93b49c39
Pawan Deep Naugain <pawan.officeal@gmail.com>
2024-02-08 14:41:22 -0600

    python file 1

---
+++ pawan1.py
@@ -0,0 +1,16 @@
+print("this is line one")
+print("and this is line 2")
+first_name = "Pawan"
+print(first_name)
+a = 10
+b = 80
+""" this is comment"""
+if a > b:
+    print("a is greater than b")
+else:
+    print("wrong answer")
+
+    x = 4
+    x = "naugain"
+    x = 123
+    print(x)
